a95c468e27da9118379c55cfa5bd48b8f9f2c5b2
ramosjanoah <ramosj.noah@gmail.com>
2018-05-04 18:35:09 +0700

    Add

--- pewdiepie/js/main_graph.js
+++ pewdiepie/js/main_graph.js
@@ -2,7 +2,7 @@
 
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 930 - margin.left - margin.right,
-    height = 360 - margin.top - margin.bottom;
+    height = 350 - margin.top - margin.bottom;
 
 var svg = d3.select("#main_graph").append("svg")
 	.attr("width", width + margin.left + margin.right)
